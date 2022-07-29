ce169bcefc92b17f6206c51fe1746417a6d2e909
heislerlemos <heisler.stlamo@gmail.com>
2022-07-29 15:15:42 +0100

    adding new user controller

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -3,9 +3,38 @@ var Userdb = require("../model/model");
 
 // create and save user 
 
-export.create  = (req, res ) => {
+exports.create  = (req, res ) => {
 
 
+	if(!req.body){
+		res.status(400).send({message: "O conteudo não pode estar vazio"})
+	}
+
+// new user
+
+const user = new Userdb ({
+	name: req.body.name,
+	email: req.body.email,
+	gender: req.body.gender,
+	status: req.body.status
+
+});
+
+//save user in the database
+
+user 
+	.save(user)
+	.then(data => {
+		res.send(data)
+	})
+
+	.catch(err => {
+		res.status(500).send(	{
+			message.err.message || "Algum erro ocorreu enquanto criavamos o user";
+		});
+	});
+	
+	
 }
 
 
