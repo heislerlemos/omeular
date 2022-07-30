54ad769c2f9355b3c70291a756c330a40edaacab
heislerlemos <heisler.stlamo@gmail.com>
2022-07-30 15:26:48 +0100

    styling the layout

--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,23 @@
-exports.homeRoutes = (req, res) => {
-		res.render("index");
+const axios = require('axios');
+
+
+
+
+exports.homeRoutes = (req, res) => {	
+	//Make a get request to the api users
+	
+	axios.get('http://localhost:3000/api/casas')
+
+		.then(function(response){
+			console.log(response.data)
+					res.render("index", {casas: response.data});
+
+		})
+
+		.catch(err => {
+			res.send(err);
+		})
+
 
 }
 
