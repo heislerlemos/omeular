3a5d266e059ce7fe89ed3d587801f5026e42668f
heislerlemos <heisler.stlano@gmail.com>
2024-03-15 11:50:37 +0100

    re authenticated the database

--- server.js
+++ server.js
@@ -8,47 +8,6 @@ const fs = require("fs");
 
 const app = express();
 
-// Basic authentication
-
-/**
-function authentication(req, res, next) {
-
-    host = "http://0.0.0.0:3000/add-casa"
-    var authheader = req.headers.authorization;
-    console.log(req.headers);
- 
-    if (!authheader) {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        return next(err)
-    }
- 
-    var auth = new Buffer.from(authheader.split(' ')[1],
-    'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
- 
-    if (user == 'admin' && pass == 'password') {
- 
-        // If Authorized user
-        next();
-    } else {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        return next(err);
-    }
- 
-}
- 
-
-
-
-// First step is the authentication of the client
-app.use(authentication)
-app.use(express.static(path.join(__dirname, 'public')));
-**/
 
 //deploying heroku
 
